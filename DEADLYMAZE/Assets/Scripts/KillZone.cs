4fe6f43aeea1f1d9a65a645fec27f59c4e5d6529
bittersweet1925 <lolojergi01@gmail.com>
2026-04-29 23:22:15 -0500

    PinchosBien

--- DEADLYMAZE/Assets/Scripts/KillZone.cs
+++ DEADLYMAZE/Assets/Scripts/KillZone.cs
@@ -3,9 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class KillZone : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        TryKill(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TryKill(other);
+    }
+
+    void TryKill(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        Collider myCol = GetComponent<Collider>();
+
+        // 🔥 detección real aunque Unity falle
+        if (myCol.bounds.Intersects(other.bounds))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
