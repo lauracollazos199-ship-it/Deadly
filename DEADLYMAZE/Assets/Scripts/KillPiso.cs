81b8888e596881494b382c02d07d334dbf49e58a
bittersweet1925 <lolojergi01@gmail.com>
2026-04-29 22:19:57 -0500

    Nivel1

--- DEADLYMAZE/Assets/Scripts/KillPiso.cs
+++ DEADLYMAZE/Assets/Scripts/KillPiso.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class KillZone : MonoBehaviour
+public class KillPiso : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
